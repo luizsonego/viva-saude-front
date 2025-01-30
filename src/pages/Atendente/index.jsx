c813d424b9aff836cf01e23cdfd21702dfc7c9b0
Luiz Yavorski <leesonego@gmail.com>
2025-01-30 18:41:03 -0300

    atendente table

--- src/pages/Atendente/index.jsx
+++ src/pages/Atendente/index.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 import { useAtendenteFetch } from "../../hooks/get/useGet.query";
 import { Link } from "react-router-dom";
-import { Button, Chip } from "@material-tailwind/react";
+import {
+  Button,
+  Card,
+  CardBody,
+  CardHeader,
+  Chip,
+  Typography,
+} from "@material-tailwind/react";
 import Table from "../Medicos/Table";
 import GenericTable from "../../components/Table/genericTable";
 
@@ -30,33 +37,45 @@ const Atendentes = () => {
         <Button>Criar novo</Button>
       </Link>
 
-      <GenericTable
-        columns={[
-          { Header: "Nome", accessor: "nome" },
-          { Header: "Email", accessor: "email" },
-          { Header: "Unidade", accessor: "unidade" },
-          {
-            Header: "cargo",
-            accessor: "cargo",
-          },
-        ]}
-        data={data?.map((item) => ({
-          id: item.id,
-          nome: <p>{item.profile.name}</p>,
-          email: <p>{item.profile.email}</p>,
-          unidade: <p>{item.profile?.unidades?.nome}</p>,
-          cargo: (
-            <Chip
-              style={{ backgroundColor: "green", height: 25 }}
-              value={item.profile.cargo}
+      <div className="mt-12 mb-8 flex flex-col gap-12">
+        <Card>
+          <CardHeader variant="gradient" color="gray" className="mb-8 p-6">
+            <Typography variant="h6" color="white">
+              Atendente
+            </Typography>
+          </CardHeader>
+          <CardBody className="overflow-x-scroll px-0 pt-0 pb-2">
+            <GenericTable
+              columns={[
+                { Header: "Nome", accessor: "nome" },
+                { Header: "Email", accessor: "email" },
+                { Header: "Unidade", accessor: "unidade" },
+                {
+                  Header: "cargo",
+                  accessor: "cargo",
+                },
+              ]}
+              data={data?.map((item) => ({
+                id: item.id,
+                nome: <p>{item.profile.name}</p>,
+                email: <p>{item.profile.email}</p>,
+                unidade: <p>{item.profile?.unidades?.nome}</p>,
+                cargo: (
+                  <Chip
+                    style={{ backgroundColor: "green", height: 25 }}
+                    value={item.profile.cargo}
+                  />
+                ),
+                resource: "prioridade",
+              }))}
+              // actionDelete={handleDelete}
+              // actionEdit={handleEdit}
+              // isDeleting={isDeleting}
             />
-          ),
-          resource: "prioridade",
-        }))}
-        // actionDelete={handleDelete}
-        // actionEdit={handleEdit}
-        // isDeleting={isDeleting}
-      />
+          </CardBody>
+        </Card>
+      </div>
+
       {/* <Table
         columns={columns}
         data={data}
